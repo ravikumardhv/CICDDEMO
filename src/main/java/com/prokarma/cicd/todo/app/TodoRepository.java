61e6f93418b1f4448d1de168664a12c1050d0717
ravikumardhv <java4u1983@gmail.com>
2016-01-05 12:22:18 -0700

    removed the source files

--- src/main/java/com/prokarma/cicd/todo/app/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package at.porscheinformatik.tutorial.todo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepository extends CrudRepository<Todo, Integer> 
-{
-}
